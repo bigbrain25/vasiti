dcd5177891ab4f3d0959a66645f7e1446cac0bf1
Anthony Oruamen <talktooruamen@gmail.com>
2021-05-01 18:59:30 +0100

    stashing

--- src/component/nav/nav.jsx
+++ src/component/nav/nav.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../../assets/images/logo.svg";
 
-const   Nav = () => {
+const Nav = () => {
   return (
     <div>
       <div className="flex">
@@ -11,9 +11,9 @@ const   Nav = () => {
         </div>
         <div className="md:flex hidden text-sm mt-10 ml-auto mr-44">
           <div className="self-center flex">
-          <Link to="/">
+            <Link to="/">
               <a className="mx-9">ABOUT US</a>
-              </Link>
+            </Link>
 
             <Link to="/stories">
               <a className="mr-9">STORIES</a>
@@ -23,7 +23,7 @@ const   Nav = () => {
             </Link>
             <Link to="/">
               <a className="mr-2">LOGIN</a>
-              </Link>
+            </Link>
           </div>
           <button
             class="bg-button-color w-20 h-6 mt-2 rounded-lg text-white"
@@ -34,12 +34,24 @@ const   Nav = () => {
         </div>
       </div>
       <div className="hidden md:flex justify-center text-sm border border-opacity-40 p-3 mt-3">
-      <div className="mx-9">MARKET PLACE</div>
-      <div className="mr-9">WHOLESALE CENTER</div>
-      <div className="mr-9">SELLER CENTER</div>
-      <div className="mr-9">SERVICES</div>
-      <div className="mr-9">INTERNSHIP</div>
-        <div>EVENTS</div>
+        <Link to="/">
+          <div className="mx-9">MARKET PLACE</div>
+        </Link>
+        <Link to="/">
+          <div className="mr-9">WHOLESALE CENTER</div>
+        </Link>
+        <Link to="/">
+          <div className="mr-9">SELLER CENTER</div>
+        </Link>
+        <Link to="/">
+          <div className="mr-9">SERVICES</div>
+        </Link>
+        <Link to="/">
+          <div className="mr-9">INTERNSHIP</div>
+        </Link>
+        <Link to="/">
+          <div>EVENTS</div>
+        </Link>
       </div>
     </div>
   );
